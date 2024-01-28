be5d991bd66ac4d92bf1dba8f3a924315793257d
Zain <zat938@gmail.com>
2024-01-28 16:22:20 +0500

    if applied, this commit updates test.yml

--- bookstore_api/bookstore_api/settings.py
+++ bookstore_api/bookstore_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
 
 # Load environment variables from the selected file
-environ.Env.read_env('.env.local')
+environ.Env.read_env('.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
